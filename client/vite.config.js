c43ae127a3886448d897e3c286a61f99e48d4d64
Diseko123 <disekoemmanuel@gmail.com>
2026-07-05 19:18:23 +0200

    Fix import syntax and update process definition
    Update vite config syntax

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   define: {
-    'process.env': {},
+    process: JSON.stringify({ env: {} }),
   },
-})
+});
